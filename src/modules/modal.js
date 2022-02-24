a6a8dc3100f03861d6a1e9a10d542c25cb60d77a
Natalia Pechatnova <pechatnova.n@yandex.ru>
2022-02-25 01:54:15 +0300

    animate modal

--- src/modules/modal.js
+++ src/modules/modal.js
@@ -2,14 +2,39 @@
 
 const modal = () => {
     const modal = document.querySelector('.popup')
+    const content = modal.querySelector('.popup-content')
     const closeBtn = modal.querySelector('.popup-close')
     const buttons = document.querySelectorAll('.popup-btn')
+    const screenWidth = window.screen.width
 
     buttons.forEach(btn => {
         btn.addEventListener('click', () => {
-            modal.style.display = 'block'
+            let count = 0;
+            let idInterval;
+
+            content.style.top = '0%'
+            const anim = () => {
+
+                if(screenWidth < 768) {
+                    modal.style.display = 'block'
+                    content.style.top = '30%'
+                } else {
+                    count++
+                    idInterval = requestAnimationFrame(anim)
+
+                    if(count <= 85) {
+                        modal.style.display = 'block'
+                        content.style.top = count*0.2 + '%'
+                    } else {
+                        cancelAnimationFrame(idInterval)
+                    }
+                }
+            }
+
+            anim()
         })
     })
+
 
     closeBtn.addEventListener('click', () => {
         modal.style.display = 'none'
