65a82124a62fc0446eb14e4c360c7762327acc12
Natalia Pechatnova <pechatnova.n@yandex.ru>
2022-03-01 11:44:37 +0300

    add slider

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import modal from "./modules/modal"
 import calculator from "./modules/calculator"
 import validations from "./modules/validations"
 import tabs from "./modules/tabs"
+import slider from "./modules/slider"
 
 
 timer('1 march 2022')
@@ -12,4 +13,5 @@ modal()
 calculator()
 validations()
 tabs()
+slider()
 
